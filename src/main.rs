0032f0c573c80c69f2878a11d611b466148ff493
vlad <ecscalibur_10@mail.ru>
2024-12-08 21:42:39 +0300

    first rust project

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
 use std::io;
 
 
-fn primes(first: i32, last: i32) -> Vec<i32> {
-    let mut res: Vec<i32> = Vec::new();
-    let current: i32 = first;
-
-    while current <= last
-    {
-        if is_prime(current) {
-            res.push(current);
-        }
-    }
-    res
-}
-
-
 fn is_prime(n: i32) -> bool {
     for i in 2..n {
         if n % i == 0{
@@ -25,11 +11,19 @@ fn is_prime(n: i32) -> bool {
 }
 
 
-fn pair_count(primes: Vec<i32>) -> i32 {
+fn pair_count(first: i32, last: i32) -> i32 {
+    if first >= last || first <= 0 || last <= 0 {
+        return 0;
+    }
+
     let mut count: i32 = 0;
-    for i in 1..primes.len() {
-        if primes[i] - primes[i-1] == 2 {
-            count += 1;
+    let mut prev: i32 = 0;
+    for i in first..last+1 {
+        if is_prime(i){
+            if prev != 0 && i - prev == 2 {
+                count += 1;
+            }
+            prev = i;
         }
     }
     count
@@ -39,10 +33,15 @@ fn pair_count(primes: Vec<i32>) -> i32 {
 fn main() {
     let mut input = String::new();
 
-    let mut m: i32;
-    let mut n: i32;
+    let m: i32;
+    let n: i32;
 
-    io::stdin().read_line(&mut input).expect("TODO: panic message");
+    io::stdin().read_line(&mut input).unwrap();
+    m = input.trim().parse::<i32>().unwrap();
 
-    println!("{}", input.trim());
+    input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    n = input.trim().parse::<i32>().unwrap();
+
+    println!("{}", pair_count(m, n));
 }
